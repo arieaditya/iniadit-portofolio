421d58698a17b6be8a3bf66bb5067c3a65cb6422
arieaditya <arieadityan@gmail.com>
2024-04-02 17:44:30 +0700

    fix: upgrade gatsby version and remove unsupported plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,7 +62,6 @@ module.exports = {
         icon: `src/images/Icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
